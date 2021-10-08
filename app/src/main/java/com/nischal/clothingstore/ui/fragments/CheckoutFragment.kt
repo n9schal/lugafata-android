d2c61994f001433e44e4902c38835f845302554a
Nischal Rai <nickelrai51@gmail.com>
2021-10-08 09:38:33 +0545

    - minor changes

--- app/src/main/java/com/nischal/clothingstore/ui/fragments/CheckoutFragment.kt
+++ app/src/main/java/com/nischal/clothingstore/ui/fragments/CheckoutFragment.kt
@@ -2,6 +2,7 @@ package com.nischal.clothingstore.ui.fragments
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import com.nischal.clothingstore.R
 import com.nischal.clothingstore.databinding.FragmentCheckoutBinding
@@ -14,6 +15,13 @@ class CheckoutFragment : Fragment(R.layout.fragment_checkout) {
         binding = FragmentCheckoutBinding.bind(view)
 
         setupToolbar()
+        setupViews()
+    }
+
+    private fun setupViews() {
+        val paymentOptions = arrayListOf<String>("Payment on Delivery", "Esewa on Delivery")
+        val adapter = ArrayAdapter(requireContext(), R.layout.layout_dropdown_item, paymentOptions)
+        binding?.etPaymentOptions?.setAdapter(adapter)
     }
 
     private fun setupToolbar() {
